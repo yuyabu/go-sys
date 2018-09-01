af9b3b4fba40516a2e9bb7544372a8478ec4ddf1
yuyabu <yuyabushita@gmail.com>
2018-09-01 20:10:53 +0900

    file sampleの実験

---
+++ chap1/filesample.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	file, err := os.Create("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	file.Write([]byte("os.File example\n"))
+	file.Close()
+}
