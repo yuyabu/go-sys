8665df70fb0328d4310bcb38ecc616db118fdc39
yuyabu <yuyabushita@gmail.com>
2018-09-16 23:44:26 +0900

    udpのクラインとを作成

---
+++ chap7/udp_cliant/ucpCliant.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("udp4", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+	fmt.Println("Sending to server")
+	_, err = conn.Write([]byte("Hello from client"))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Receiving from server")
+	buffer := make([]byte, 1500)
+	length, err := conn.Read(buffer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Received: %s\n", string(buffer[:length]))
+}
