61e4f5e944c77bc64baeccd462528e0978d2e06a
yuyabu <yuyabushita@gmail.com>
2018-10-07 21:55:05 +0900

    pidのサンプル追加

---
+++ chap11/process/process.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	fmt.Printf("プロセスID:%d\n", os.Getpid)
+	fmt.Printf("親プロセスID:%d\n", os.Getppid)
+	fmt.Printf("user_id:%d\n", os.Geteuid)
+	fmt.Printf("group_id:%d\n", os.Getegid)
+	fmt.Printf("\n")
+	for {
+		fmt.Println(".")
+		time.Sleep(1 * time.Second)
+	}
+}
