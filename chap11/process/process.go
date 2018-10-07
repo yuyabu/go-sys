b1c5cfcce094efe7fba0f48bdd20aaac9ed72e0b
yuyabu <yuyabushita@gmail.com>
2018-10-07 22:03:03 +0900

    fix typo
    関数呼び出しの()のつけ忘れ

--- chap11/process/process.go
+++ chap11/process/process.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	fmt.Printf("プロセスID:%d\n", os.Getpid)
-	fmt.Printf("親プロセスID:%d\n", os.Getppid)
-	fmt.Printf("user_id:%d\n", os.Geteuid)
-	fmt.Printf("group_id:%d\n", os.Getegid)
+	fmt.Printf("プロセスID:%d\n", os.Getpid())
+	fmt.Printf("親プロセスID:%d\n", os.Getppid())
+	fmt.Printf("user_id:%d\n", os.Geteuid())
+	fmt.Printf("group_id:%d\n", os.Getegid())
 	fmt.Printf("\n")
 	for {
 		fmt.Println(".")
